9162317f6052b7bddf0c1213dcdff7b41847d784
Jin-zhou-s <3114287158@qq.com>
2024-03-25 17:08:24 +0000

    任务1,2,3,4完成

--- rental.py
+++ rental.py
@@ -5,9 +5,10 @@ import Pyro5.api
 class rental(object):
     def __init__(self):
         self.users = []
+        self.manufacturers = []
 
     def add_user(self, user_name, user_number):
-        for repeat_name, repeat_number, in self.users:
+        for repeat_name, repeat_number in self.users:
             if user_name == repeat_name:
                 return False
         self.users.append((user_name, user_number))
@@ -21,6 +22,22 @@ class rental(object):
             for user_name, user_number in self.users:
                 index.append(f"User name: {user_name}, User number: {user_number}")
             return "\n".join(index)
+
+    def add_manufacturer(self, manufacturer_name, manufacturer_country):
+        for repeat_manufacturer_name, repeat_manufacturer_country in self.manufacturers:
+            if manufacturer_name == repeat_manufacturer_name:
+                return False
+        self.manufacturers.append((manufacturer_name, manufacturer_country))
+        return True
+
+    def return_manufacturers(self):
+        if not self.manufacturers:
+            return "not have manufacturer"
+        else:
+            array = []
+            for manufacturer_name, manufacturer_country in self.manufacturers:
+                array.append(f"manufacturer name:{manufacturer_name},  manufacturer country: {manufacturer_country}")
+            return "\n".join(array)
 
 
 def main():
